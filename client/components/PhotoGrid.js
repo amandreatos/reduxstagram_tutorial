340328f9a547a5417e9b4f4860a0454ee7f599c6
Amandreatos <anthony@321webmarketing.com>
2018-06-22 16:40:11 -0400

    initial commit

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Photo from "./Photo";
+
+const PhotoGrid = React.createClass({
+  render() {
+    return (
+      <div className="photo-grid">
+        {this.props.posts.map((post, i) => (
+          <Photo key={i} i={i} {...this.props} post={post} />
+        ))}
+      </div>
+    );
+  }
+});
+
+export default PhotoGrid;
